20a589fc610b2aaf3cefc41e34baab74c3973f9d
Enoch768 <102437091+Enoch768@users.noreply.github.com>
2023-11-24 12:34:57 +0100

    Add files via upload

---
+++ speed.py
@@ -0,0 +1,24 @@
+# app.py
+import streamlit as st
+
+st.markdown("<h1 style='text-align: center; color: #0000FF;'>Speed Limit Detection</h1>", unsafe_allow_html=True)
+
+st.markdown("<p style='text-align: center; font-size:22px;'>This is a app that detect the speed limit</p>", unsafe_allow_html=True)
+
+# Add a text input
+user_input = st.number_input("Enter the Speed:", min_value=0, max_value=None, value=0, step=1)
+
+# Display the input
+st.write("The Speed Entered:", user_input, "Km/h")
+if user_input > 40:
+    st.markdown("<div style='display: flex; justify-content: center;'>"
+                    "<img src='https://upload.wikimedia.org/wikipedia/commons/1/17/Warning.svg' alt='Warning Image'></div>",
+                    unsafe_allow_html=True)
+    st.markdown("<p style='text-align: center; font-size:22px;'>You have exceeded the speed limit of 40km/h !!!!!!</p>", unsafe_allow_html=True)
+else:
+    st.markdown("<div style='display: flex; justify-content: center; height: 200px; width: 80%; background-color: None;'>"
+                    "<img src='https://miro.medium.com/v2/resize:fit:720/format:webp/1*lpWX1QftoVYDZDYdsGhEtg.jpeg' alt='Warning Image'></div>",
+                    unsafe_allow_html=True)
+    st.markdown("<p style='font-size:22px; text-indent: 40px;'>You have not exceeded the speed limit of 40km/h !!!!!!</p>", unsafe_allow_html=True)
+
+
